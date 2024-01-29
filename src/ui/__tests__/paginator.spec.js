d41c074a9490d9b6d0044d648599a1868ec7e66f
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2024-01-29 17:21:52 -0300

    pruebas de codigo
    logre terminar las pruebas de paginador.js y solo me queda hacer las pruebas de pokemon.

--- src/ui/__tests__/paginator.spec.js
+++ src/ui/__tests__/paginator.spec.js
@@ -3,12 +3,15 @@ import paginaDos from "../../../cypress/fixtures/listado-pagina-2.json"
 import paginaCyn from "../../../cypress/fixtures/listado-pagina-49.json"
 import pokedexFixture from "../../__tests__/pokedex.fixture"
 import mostrarPaginador from "../paginador"
+import { manejarCambioPagina } from "../paginador"
 
-test("mostrador de pagina", () => {
+test("mostrador de pagina 1", () => {
     document.body.innerHTML = pokedexFixture
     mostrarPaginador(
         964,
         1,
+        paginaUno.next,
+        paginaUno.previous,
         () => {}
     )
     const $paginador = document.querySelector("#paginador")
@@ -16,12 +19,38 @@ test("mostrador de pagina", () => {
     $paginador.dispatchEvent(click)
 })
 
-test("mosrador de pagina 2", () => {
+test("mostrador de pagina 2", () => {
+    document.body.innerHTML = pokedexFixture
+    mostrarPaginador(
+        964,
+        2,
+        paginaDos.next,
+        paginaDos.previous
+    )
+})
+
+test("mostrador de pagina 49", () => {
     document.body.innerHTML = pokedexFixture
     mostrarPaginador(
         964,
         49,
         paginaCyn.next,
-        paginaDos.next,
+        paginaCyn.previous
     )
 })
+
+test("manejar el cambio de pagina", () => {
+    const callbackPaginaSeleccionadaMock = jest.fn()
+    const eventoMock = {
+        preventDefault: jest.fn(),
+        target: {
+          getAttribute: jest.fn(() => '#'),
+          dataset: { pagina: '2' },
+        },
+      };
+    
+      manejarCambioPagina(eventoMock, callbackPaginaSeleccionadaMock);
+    
+      expect(eventoMock.preventDefault).toHaveBeenCalled();
+      expect(callbackPaginaSeleccionadaMock).toHaveBeenCalledWith(2);
+    });
