ca3b3d4ad5756ac6acd7b44f3107d68e4075d2a0
lautaroPistagnesi <lautaroPistagnesisobelvio@gmail.com>
2024-02-01 17:49:36 -0300

    pruebas de jest
    casi termine las pruebas de paginador, pero por alguna razon aun queda una linea que no tengo idea de como testearla, por eso decidi seguir con el archivo de pokemon para no perder tanto tiempo.

--- src/ui/__tests__/paginator.spec.js
+++ src/ui/__tests__/paginator.spec.js
@@ -2,21 +2,25 @@ import paginaUno from "../../../cypress/fixtures/listado-pagina-1.json"
 import paginaDos from "../../../cypress/fixtures/listado-pagina-2.json"
 import paginaCyn from "../../../cypress/fixtures/listado-pagina-49.json"
 import pokedexFixture from "../../__tests__/pokedex.fixture"
-import mostrarPaginador from "../paginador"
-import { manejarCambioPagina } from "../paginador"
+import mostrarPaginador from "../paginador.js"
+import { manejarCambioPagina } from "../paginador.js"
 
 test("mostrador de pagina 1", () => {
     document.body.innerHTML = pokedexFixture
+    const callbackPaginaSeleccionada = jest.fn()
     mostrarPaginador(
         964,
         1,
         paginaUno.next,
         paginaUno.previous,
-        () => {}
+        callbackPaginaSeleccionada,
     )
     const $paginador = document.querySelector("#paginador")
     const click = new Event("click")
     $paginador.dispatchEvent(click)
+    expect(document.querySelectorAll("li")[0].className).toBe("page-item disabled")
+    expect(document.querySelectorAll("li")[50].className).toBe("page-item")
+    expect(callbackPaginaSeleccionada).toHaveBeenCalled()
 })
 
 test("mostrador de pagina 2", () => {
@@ -25,8 +29,10 @@ test("mostrador de pagina 2", () => {
         964,
         2,
         paginaDos.next,
-        paginaDos.previous
+        paginaDos.previous,
     )
+    expect(document.querySelectorAll("li")[0].className).toBe("page-item")
+    expect(document.querySelectorAll("li")[50].className).toBe("page-item")
 })
 
 test("mostrador de pagina 49", () => {
@@ -35,22 +41,38 @@ test("mostrador de pagina 49", () => {
         964,
         49,
         paginaCyn.next,
-        paginaCyn.previous
+        paginaCyn.previous,
     )
+    expect(document.querySelectorAll("li")[0].className).toBe("page-item")
+    expect(document.querySelectorAll("li")[50].className).toBe("page-item disabled")
 })
 
-test("manejar el cambio de pagina", () => {
-    const callbackPaginaSeleccionadaMock = jest.fn()
+test('manejarCambioPagina llama a callbackPaginaSeleccionada', () => {
+    const callbackPaginaSeleccionadaMock = jest.fn();
     const eventoMock = {
-        preventDefault: jest.fn(),
-        target: {
-          getAttribute: jest.fn(() => '#'),
-          dataset: { pagina: '2' },
-        },
-      };
-    
-      manejarCambioPagina(eventoMock, callbackPaginaSeleccionadaMock);
-    
-      expect(eventoMock.preventDefault).toHaveBeenCalled();
-      expect(callbackPaginaSeleccionadaMock).toHaveBeenCalledWith(2);
-    });
+      preventDefault: jest.fn(),
+      target: {
+        getAttribute: jest.fn(() => '#'),
+        dataset: { pagina: '2' },
+      },
+    };
+  
+    manejarCambioPagina(eventoMock, callbackPaginaSeleccionadaMock);
+
+    expect(eventoMock.preventDefault).toHaveBeenCalled();
+    expect(callbackPaginaSeleccionadaMock).toHaveBeenCalledWith(2);
+  });
+
+  test('manejarCambioPagina llama a callbackPaginaSeleccionada', () => {
+    const eventoMock = {
+      preventDefault: jest.fn(),
+      target: {
+        getAttribute: jest.fn(() => '#'),
+        dataset: { pagina: '2' },
+      },
+    };
+  
+    manejarCambioPagina(eventoMock);
+
+    expect(eventoMock.preventDefault).toHaveBeenCalled();
+  });
--- src/ui/__tests__/pokemon.spec.js
+++ src/ui/__tests__/pokemon.spec.js
@@ -1,3 +1,16 @@
-test("nada", ()=>{
+import mostrarPokemon from "../pokemon";
+import bulbasaur from "../../../cypress/fixtures/bulbasaur.json"
+import pokedexFixture from "../../__tests__/pokedex.fixture";
+
+test("mostrar pokemon", () => {
+    document.body.innerHTML = pokedexFixture
+    const pokemon = {
+        id:bulbasaur.id,
+        nombre:bulbasaur.name,
+        foto:bulbasaur.sprites.front_default,
+        tipos:bulbasaur.types,
+        habilidades:bulbasaur.abilities,
+        movimientos:bulbasaur.moves,
+    }
     
 })
